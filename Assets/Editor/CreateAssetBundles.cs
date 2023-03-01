3c675cd949d9d32a1cbbdfcc17b2b92120329d8f
MengZhangxi <liulimengplay@163.com>
2023-03-01 11:27:40 +0800

    bundle构建完成后对循环依赖进行判断并输出警告

--- Assets/Editor/CreateAssetBundles.cs
+++ Assets/Editor/CreateAssetBundles.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.IO;
 using UnityEditor;
 using UnityEngine;
@@ -16,9 +17,27 @@ public class CreateAssetBundles
                                         BuildAssetBundleOptions.None,
                                         BuildTarget.StandaloneWindows);
 
+        CheckCircularDependenciesAndWarning(manifest);
+
         AssetBundleBuild bundlesInfoBuild = CreateBundlesInfoBuild(assetBundleDirectory, manifest);
         BuildPipeline.BuildAssetBundles(assetBundleDirectory, new AssetBundleBuild[] { bundlesInfoBuild }, BuildAssetBundleOptions.None,
             BuildTarget.StandaloneWindows);
+    }
+
+    private static void CheckCircularDependenciesAndWarning(AssetBundleManifest manifest)
+    {
+        var circularDependencies = CheckCircularDependencies(manifest);
+        if (circularDependencies != null && circularDependencies.Count > 0)
+        {
+            for (int i = 0; i < circularDependencies.Count; i++)
+            {
+                var circularDependency = circularDependencies[i];
+                string logInfo = "Circular Dependency " + i + ": ";
+                foreach (var dependency in circularDependency)
+                    logInfo += dependency + "->";
+                Debug.LogWarning(logInfo);
+            }
+        }
     }
 
     private static AssetBundleBuild CreateBundlesInfoBuild(string assetBundleDirectory, AssetBundleManifest manifest)
@@ -44,5 +63,61 @@ public class CreateAssetBundles
             assetNames = new string[] { assetName },
         };
         return bundlesInfoBuild;
+    }
+
+    private static List<List<string>> CheckCircularDependencies(AssetBundleManifest manifest)
+    {
+        Dictionary<string, HashSet<string>> dependencyGraph = new Dictionary<string, HashSet<string>>();
+
+        string[] bundleNames = manifest.GetAllAssetBundles();
+        foreach (string bundleName in bundleNames)
+        {
+            string[] dependencies = manifest.GetDirectDependencies(bundleName);
+            dependencyGraph.Add(bundleName, new HashSet<string>(dependencies));
+        }
+
+        List<List<string>> circularDependencies = new List<List<string>>();
+
+        foreach (string bundleName in bundleNames)
+        {
+            Stack<string> path = new Stack<string>();
+            HashSet<string> visited = new HashSet<string>();
+            CheckCircularDependenciesRecursive(bundleName, path, visited, dependencyGraph, circularDependencies);
+        }
+
+        return circularDependencies;
+    }
+
+    private static void CheckCircularDependenciesRecursive(string bundleName, Stack<string> path, HashSet<string> visited, Dictionary<string, HashSet<string>> dependencyGraph, List<List<string>> circularDependencies)
+    {
+        if (visited.Contains(bundleName))
+        {
+            if (path.Contains(bundleName))
+            {
+                List<string> circularPath = new List<string>();
+                circularPath.AddRange(path);
+                circularPath.Reverse();
+                circularPath.Add(bundleName);
+                circularDependencies.Add(circularPath);
+            }
+            return;
+        }
+
+        visited.Add(bundleName);
+        path.Push(bundleName);
+
+        HashSet<string> dependencies;
+        if (!dependencyGraph.TryGetValue(bundleName, out dependencies))
+        {
+            dependencies = new HashSet<string>();
+        }
+
+        foreach (string dependency in dependencies)
+        {
+            CheckCircularDependenciesRecursive(dependency, path, visited, dependencyGraph, circularDependencies);
+        }
+
+        path.Pop();
+        visited.Remove(bundleName);
     }
 }
